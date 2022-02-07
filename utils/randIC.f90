393242300bfb063fd48e6f8a3c5500208cb03c5f
Santiago Benavides <santib@mit.edu>
2022-02-07 15:40:16 -0500

    Added a line which checks to see if you're restarting from zero. If not, then skips the generation of a new initial condition.

--- utils/randIC.f90
+++ utils/randIC.f90
@@ -21,10 +21,15 @@ PROGRAM double
   call var_precompute()
   call tra_precompute()
   call  io_precompute()
-
-  call var_randmpt(vel_c)
-  call var_maskmpt(vel_c)
-  call io_save_state()
+    
+  if (io_save1==0) then
+     call var_randmpt(vel_c)
+     call var_maskmpt(vel_c)
+     call io_save_state()
+  else
+     if (mpi_rnk==0) print*, 'NOT starting from zero, therefore not generating a
+     new random initial condition.'
+  endif
 
 #ifdef _MPI
   call mpi_barrier(mpi_comm_world, mpi_er)
