f17433b470986f780b321685ec1095e79f5f02f2
Santiago Benavides <santib@mit.edu>
2022-04-07 12:22:59 -0400

    Added two new ways of calculating Reynolds stress, one that is based on time-averaging the full field (vel_restress_2d_calc) and one that is based on a instantaneous spatial filtering (vel_restress_filt).

--- program/velocity.f90
+++ program/velocity.f90
@@ -14,6 +14,8 @@ module velocity
   
   type (phys) :: vel_p
   type (mpt)  :: vel_c,vel_c2,vel_onl,vel_nl,lhs,rhs
+  type(spec) :: umean_2d,remean1_2d,remean2_2d 
+  !umean_2d = (umean,vmean,wmean), remean1_2d = (uumean,uvmean,uwmean), remean2_2d = (vvmean,wvmean,wwmean)
   type(spec_xavg_even) :: umean,wmean,uumean,uwmean,wwmean,vvmean
   type(spec_xavg_odd) :: vmean,uvmean,wvmean
   logical :: nst
@@ -178,7 +180,7 @@ contains
     
   end subroutine vel_history
 
-  subroutine vel_restress_calc()
+  subroutine vel_restress_calc(i_count)
   !! This subroutine takes the current fields and updates the average values and the Reynolds stresses
   !! These are x-averaged, and the time-mean is updated.
   !! Uses global variables: i_count, vel_c as inputs. and umean,vmean,wmean,
@@ -189,13 +191,10 @@ contains
     type(spec_xavg_even) :: ubar,wbar,uubar,uwbar,wwbar,vvbar
     type(spec_xavg_odd) :: vbar,uvbar,wvbar
     type(phys) :: p,mult_aux
+    integer, intent(in) ::  i_count
     _loop_mn_vars
 
-    ! Updates the count and averaging window
-    i_count = i_count + 1
-    d_avg_time = d_avg_time + d_dt
-
-    ! Takes current mpt field and converts to phys
+    ! Takes current mpt field and converts to spec
     call var_mpt2spec(vel_c,u)
     
     ! -------- MEAN VELOCITIES -------
@@ -311,6 +310,172 @@ contains
     vvmean%Re = 0.0*vvmean%Re
     vvmean%Im = 0.0*vvmean%Im
   end subroutine vel_restress_reset
+
+  subroutine vel_restress_2d_calc(i_count)
+  !! This subroutine takes the current fields and updates the average values and the Reynolds stresses
+  !! These are full 2d, and the time-mean is updated.
+  !! Uses global variables: i_count, vel_c as inputs. and umean_2d,vmean_2d,wmean_2d
+  !! uumean_2d,uvmean_2d,uwmean_2d,vvmean_2d,wvmean_2d,wwmean_2d as outputs. The outputs are of the
+  !! type 'spec' 
+
+    type(spec) :: u,up,nl_tmp
+    type(phys) :: p,mult_aux
+    integer, intent(in) :: i_count
+    _loop_mn_vars
+
+    ! Takes current mpt field and converts to spec
+    call var_mpt2spec(vel_c,u)
+    
+    ! -------- MEAN VELOCITIES -------
+
+    ! Then update the mean
+    umean_2d%Re = umean_2d%Re + (u%Re - umean_2d%Re)/i_count 
+    umean_2d%Im = umean_2d%Im + (u%Im - umean_2d%Im)/i_count  
+
+    ! -------- REYNOLDS STRESSES  -------
+    ! <u'u'>, <u'v'> and <u'w'>
+    ! Calculate u' = u-umean, etc
+    up%Re = u%Re - umean_2d%Re
+    up%Im = u%Im - umean_2d%Im
+    
+    ! Calculate u'u', u'v', u'w'
+    call tra_spec2phys(up,p)
+
+    _loop_phy_begin
+    mult_aux%Re(1:i_K0,n,m)=nluw(p%Re(1:i_K0,n,m),p%Re(1:i_K0,n,m)) ! u'u'
+    mult_aux%Re(i_K0+1:2*i_K0-1,n,m)=nluv(p%Re(1:i_K0,n,m),p%Re(i_K0+1:2*i_K0-1,n,m)) ! u'v'
+    mult_aux%Re(2*i_K0:3*i_K0-1,n,m)=nluw(p%Re(1:i_K0,n,m),p%Re(2*i_K0:3*i_K0-1,n,m)) ! u'w'
+    _loop_mn_end
+    
+    call tra_phys2spec(mult_aux,nl_tmp)
+
+    ! Update the mean
+    remean1_2d%Re = remean1_2d%Re + (nl_tmp%Re - remean1_2d%Re)/i_count 
+    remean1_2d%Im = remean1_2d%Im + (nl_tmp%Im - remean1_2d%Im)/i_count 
+
+    ! Let's do the same, but for w'w', v'w'
+    _loop_phy_begin
+    mult_aux%Re(1:i_K0,n,m)=nlvv(p%Re(i_K0+1:2*i_K0-1,n,m)) ! v'v'
+    mult_aux%Re(i_K0+1:2*i_K0-1,n,m)=nluv(p%Re(2*i_K0:3*i_K0-1,n,m),p%Re(i_K0+1:2*i_K0-1,n,m)) ! w'v'
+    mult_aux%Re(2*i_K0:3*i_K0-1,n,m)=nluw(p%Re(2*i_K0:3*i_K0-1,n,m),p%Re(2*i_K0:3*i_K0-1,n,m)) ! w'w'
+    _loop_mn_end
+    
+    call tra_phys2spec(mult_aux,nl_tmp)
+    
+    ! Update the mean
+    remean2_2d%Re = remean2_2d%Re + (nl_tmp%Re - remean2_2d%Re)/i_count 
+    remean2_2d%Im = remean2_2d%Im + (nl_tmp%Im - remean2_2d%Im)/i_count 
+
+  end subroutine vel_restress_2d_calc
+
+  subroutine vel_restress_2d_reset()
+    umean_2d%Re = 0.0*umean_2d%Re
+    umean_2d%Im = 0.0*umean_2d%Im
+    remean1_2d%Re = 0.0*remean1_2d%Re
+    remean1_2d%Im = 0.0*remean1_2d%Im
+    remean2_2d%Re = 0.0*remean2_2d%Re
+    remean2_2d%Im = 0.0*remean2_2d%Im
+  end subroutine vel_restress_2d_reset
+  
+  subroutine vel_restress_filt_calc(vel_c,l_c,umean_filt,remean1_filt,remean2_filt)
+  !! This subroutine takes the current fields and calculates the mean
+  !! via lowpass filter. Then calculates Reynolds averages.
+  !! These are full 2d in x-z plane. No x-averaging
+  !! Inputs: vel_c (mpt), cutoff length l_c
+  !! Outputs: filtered mean velocity umean_filt, and reynolds stresses
+  !! remean1_filt, remean2_filt (all of spec type)
+
+    type(mpt), intent(in) :: vel_c
+    type(spec) :: u,up,nl_tmp
+    type(spec), intent(out) :: umean_filt,remean1_filt,remean2_filt
+    type(phys) :: p,mult_aux
+    double precision, intent(in) :: l_c
+    double precision :: k_c,k
+    _loop_mn_vars
+
+    ! Takes current mpt field and converts to spec
+    call var_mpt2spec(vel_c,u)
+    
+    ! -------- MEAN VELOCITIES -------
+
+    k_c = 2.0d0*d_PI/l_c
+
+    ! Filter the velocity field
+    _loop_mn_begin
+    if (m<i_MM) then
+        k = sqrt((d_alpha*m)**2+(d_gamma*n)**2)
+    else if (m.ge.i_MM) then
+        k = sqrt((d_alpha*(m-i_M))**2+(d_gamma*n)**2)
+    end if 
+    if (k.ge.k_c) then
+        umean_filt%Re(:,m,n) = 0.0*u%Re(:,m,n) 
+        umean_filt%Im(:,m,n) = 0.0*u%Im(:,m,n)
+    else
+        umean_filt%Re(:,m,n) = u%Re(:,m,n) 
+        umean_filt%Im(:,m,n) = u%Im(:,m,n)
+    end if
+    _loop_mn_end
+
+    ! -------- REYNOLDS STRESSES  -------
+    ! <u'u'>, <u'v'> and <u'w'>
+    ! Calculate u' = u-umean, etc
+    up%Re = u%Re - umean_filt%Re
+    up%Im = u%Im - umean_filt%Im
+    
+    ! Calculate u'u', u'v', u'w'
+    call tra_spec2phys(up,p)
+
+    _loop_phy_begin
+    mult_aux%Re(1:i_K0,n,m)=nluw(p%Re(1:i_K0,n,m),p%Re(1:i_K0,n,m)) ! u'u'
+    mult_aux%Re(i_K0+1:2*i_K0-1,n,m)=nluv(p%Re(1:i_K0,n,m),p%Re(i_K0+1:2*i_K0-1,n,m)) ! u'v'
+    mult_aux%Re(2*i_K0:3*i_K0-1,n,m)=nluw(p%Re(1:i_K0,n,m),p%Re(2*i_K0:3*i_K0-1,n,m)) ! u'w'
+    _loop_mn_end
+    
+    call tra_phys2spec(mult_aux,nl_tmp)
+
+    ! Filter 
+    _loop_mn_begin
+    if (m<i_MM) then
+        k = sqrt((d_alpha*m)**2+(d_gamma*n)**2)
+    else if (m.ge.i_MM) then
+        k = sqrt((d_alpha*(m-i_M))**2+(d_gamma*n)**2)
+    end if 
+    if (k.ge.k_c) then
+        remean1_filt%Re(:,m,n) = 0.0*nl_tmp%Re(:,m,n) 
+        remean1_filt%Im(:,m,n) = 0.0*nl_tmp%Im(:,m,n)
+    else
+        remean1_filt%Re(:,m,n) = nl_tmp%Re(:,m,n) 
+        remean1_filt%Im(:,m,n) = nl_tmp%Im(:,m,n)
+    end if
+    _loop_mn_end
+
+    ! Let's do the same, but for w'w', v'w'
+    _loop_phy_begin
+    mult_aux%Re(1:i_K0,n,m)=nlvv(p%Re(i_K0+1:2*i_K0-1,n,m)) ! v'v'
+    mult_aux%Re(i_K0+1:2*i_K0-1,n,m)=nluv(p%Re(2*i_K0:3*i_K0-1,n,m),p%Re(i_K0+1:2*i_K0-1,n,m)) ! w'v'
+    mult_aux%Re(2*i_K0:3*i_K0-1,n,m)=nluw(p%Re(2*i_K0:3*i_K0-1,n,m),p%Re(2*i_K0:3*i_K0-1,n,m)) ! w'w'
+    _loop_mn_end
+    
+    call tra_phys2spec(mult_aux,nl_tmp)
+    
+    ! Update the mean
+    ! Filter 
+    _loop_mn_begin
+    if (m<i_MM) then
+        k = sqrt((d_alpha*m)**2+(d_gamma*n)**2)
+    else if (m.ge.i_MM) then
+        k = sqrt((d_alpha*(m-i_M))**2+(d_gamma*n)**2)
+    end if 
+    if (k.ge.k_c) then
+        remean2_filt%Re(:,m,n) = 0.0*nl_tmp%Re(:,m,n) 
+        remean2_filt%Im(:,m,n) = 0.0*nl_tmp%Im(:,m,n)
+    else
+        remean2_filt%Re(:,m,n) = nl_tmp%Re(:,m,n) 
+        remean2_filt%Im(:,m,n) = nl_tmp%Im(:,m,n)
+    end if
+    _loop_mn_end
+
+  end subroutine vel_restress_filt_calc
 
 end module velocity
 
