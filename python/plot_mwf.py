f2ded73bc162e3e52e0a4c24f6ab7dbc9162476f
Santiago Benavides <santib@mit.edu>
2022-06-21 11:49:37 -0400

    Minor changes to plotting script.

--- python/plot_mwf.py
+++ python/plot_mwf.py
@@ -14,6 +14,8 @@ idir = '../'
 odir = './Figures/'
 
 # Searches through all directories in 'Data' folder (which are named after experiments) and imports the data:
+#dirs = sorted(glob.glob(idir+'MFU*'))
+#dirs = sorted(glob.glob(idir+'spot_test*'))
 dirs = sorted(glob.glob(idir+'Lx*'))
 
 runs = []
@@ -48,7 +50,7 @@ for run in runs[:]:
     ke=np.genfromtxt(idir+run+'/vel_energy.dat')[:,1]
     print('initial KE = %s' % ke[0])
 #     print("length = %s" % len(ke))
-    plt.semilogy(t,ke,label=run)
+    plt.semilogy(ke,label=run)
     
 # plt.title(run)
 # print('max t = %s' % np.max(t))
@@ -64,11 +66,50 @@ plt.show()
 for ii,run in enumerate(runs):
     print(run)
     state_outs = sorted(glob.glob(idir+run+'/state*'))
-#     print(state_outs[:])
-    for state_out in np.array(state_outs)[-2:]:
-        print(state_out)
+##     print(state_outs[:])
+    for state_out in np.array(state_outs)[-1:]:
+#        print(state_out)
         aa.plot_XZ(state_out,'U',0.0,figwidth=10)
-        
+        aa.plot_XZ(state_out,'V',0.0,figwidth=10)
+        aa.plot_XZ(state_out,'W',0.0,figwidth=10)
+        aa.plot_YZ_xavg(state_out,'U',Ny=100,figwidth=24)
+        aa.plot_YZ_xavg(state_out,'V',Ny=100,figwidth=24)
+        aa.plot_YZ_xavg(state_out,'W',Ny=100,figwidth=24)
+
+
+#     spec_in = sorted(glob.glob(idir+run+'/spec.cdf*'))
+#     if len(spec_in)>0:
+#         aa.plot_YZ_xavg(spec_in[-1],'U',intype='spec',figwidth=24,Ny=100)
+#         aa.plot_YZ_xavg(spec_in[-1],'V',intype='spec',figwidth=24,Ny=100)
+#         aa.plot_YZ_xavg(spec_in[-1],'W',intype='spec',figwidth=24,Ny=100)
+    re_in = sorted(glob.glob(idir+run+'/restress_2d*'))
+    if len(re_in)>0:
+        aa.plot_YZ_xavg(re_in[-1],'U',intype='umean_2d',Retype='2d',figwidth=24,Ny=100)
+        aa.plot_XZ(re_in[-1],'U',0.0,intype='umean_2d',Retype='2d',figwidth=10)
+        aa.plot_YZ_xavg(re_in[-1],'V',intype='umean_2d',Retype='2d',figwidth=24,Ny=100)
+        aa.plot_YZ_xavg(re_in[-1],'W',intype='umean_2d',Retype='2d',figwidth=24,Ny=100)
+# #        aa.plot_YZ_xavg(re_in[-1],'U',intype='SpecReForces',Retype='2d',figwidth=24,Ny=100)
+# #        aa.plot_YZ_xavg(re_in[-1],'V',intype='SpecReForces',Retype='2d',figwidth=24,Ny=100)
+# #        aa.plot_YZ_xavg(re_in[-1],'W',intype='SpecReForces',Retype='2d',figwidth=24,Ny=100)
+        time,Re,Lx,Lz,specF = aa.load_spec(re_in[-1],intype = 'SpecReForces',Retype='2d')
+        F,F_ic = aa.div_comp(Lx,Lz,specF)       
+        figwidth=24
+        aspect=1
+        for field in ['U','V','W']:  
+            time,Re,Lx,Lz,Y,Z,r = aa.GridXavg(time,Re,Lx,Lz,F_ic,field,Ny=100)
+          
+            rbar = np.max([-np.min(r),np.max(r)])
+          
+            plt.figure(figsize=(figwidth,figwidth/3))
+            plt.title("F_ic, Re = %.2e, Lx = %.2f, Lz = %.2f, \n Field: %s, time= %.4f" % (Re,Lx,Lz,field,time))
+            plt.pcolor(Z,Y,r,vmin=-rbar,vmax=rbar,cmap='bwr')
+            ax=plt.gca()
+            ax.set_aspect(aspect)
+            plt.colorbar()
+            plt.ylabel(r'$y$')
+            plt.xlabel(r'$z$')
+            plt.show()
+#
 # # figwidths=[10,10,10]
 # for ii,run in enumerate(runs):
 #     print(run)
@@ -80,23 +121,23 @@ for ii,run in enumerate(runs):
 ### Spectra ###
 ###############
 
-for ii,run in enumerate(runs):
-#     print(run)
-    if '_1024' in run:
-        Nx = 1024
-        Nz = 512
-    else:
-        Nx = 512
-        Nz = 256
-    vel_specs = sorted(glob.glob(idir+run+'/vel_sp*'))
-    ks,spec=np.genfromtxt(vel_specs[-1]).T
-    kxs = ks[:int(Nx/2)]
-    kzs = ks[int(Nx/2):]
-    KEx = spec[:int(Nx/2)]
-    KEz = spec[int(Nx/2):]
-    plt.loglog(kxs,KEx,label='KEx')
-    plt.loglog(kzs,KEz,label='KEz')
-    plt.title(run)
-    plt.legend()
-    plt.ylim(np.max([1e-13,np.min([np.min(KEx),np.min(KEz)])]),5*np.max([np.max(KEx),np.max(KEz)]))
-    plt.show()
+#for ii,run in enumerate(runs):
+##     print(run)
+#    if '_1024' in run:
+#        Nx = 1024
+#        Nz = 512
+#    else:
+#        Nx = 512
+#        Nz = 256
+#    vel_specs = sorted(glob.glob(idir+run+'/vel_sp*'))
+#    ks,spec=np.genfromtxt(vel_specs[-1]).T
+#    kxs = ks[:int(Nx/2)]
+#    kzs = ks[int(Nx/2):]
+#    KEx = spec[:int(Nx/2)]
+#    KEz = spec[int(Nx/2):]
+#    plt.loglog(kxs,KEx,label='KEx')
+#    plt.loglog(kzs,KEz,label='KEz')
+#    plt.title(run)
+#    plt.legend()
+#    plt.ylim(np.max([1e-13,np.min([np.min(KEx),np.min(KEz)])]),5*np.max([np.max(KEx),np.max(KEz)]))
+#    plt.show()
